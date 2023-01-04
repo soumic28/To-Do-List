073cf4659a8994aa8a57875727393af60a079bd0
soumic28 <94804510+soumic28@users.noreply.github.com>
2023-01-04 14:34:51 +0530

    upated the port

--- index.js
+++ index.js
@@ -45,6 +45,8 @@ app.get("/about",function(req,res){
 
 
 
-app.listen(3000,function(){
-    console.log("Server is running");
-});
+app.listen(process.env.PORT || 3000 ,function(){
+    console.log("Server is running on port 3000");
+
+
+});    
